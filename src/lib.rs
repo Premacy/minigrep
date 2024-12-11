9d34b9d0640e559af9d8cd38f9844531aa4a8dc4
Premacy <myemail@.c>
2024-12-11 16:57:19 +0700

    Add search function

--- src/lib.rs
+++ src/lib.rs
@@ -9,22 +9,26 @@ mod tests {
     fn one_result(){
         let query = "duct";
         let contents = "\
-        Rust:
-        safe,fast,productive.
-        Pick three.
-        ";
+Rust:
+safe, fast, productive.
+Pick three.
+";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents)); 
     }
 }
 
-// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-//     for line in contents.lines() {
-//         if line.contains(query) {
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
 
-//         }
-//     }
-// }
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
 
 pub struct Config{
     pub query: String,
